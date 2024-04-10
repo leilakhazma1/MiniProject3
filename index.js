17a913fd2d260fbed872999299c7180c2f46c782
leilakhazma1 <leilakhazma7@gmail.com>
2024-04-10 10:10:50 +1000

    body parser

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const breedRoutes = require('./routes/breedRoutes');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+// add all the routes here 
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
 
