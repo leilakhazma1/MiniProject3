e1d8ed7c3384ab01c3b806fac74730eb8140eb71
leilakhazma1 <leilakhazma7@gmail.com>
2024-04-10 12:06:13 +1000

    index

--- index.js
+++ index.js
@@ -1,60 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const pool = require('./db');
+const breedRoutes = require('./routes/breedRoutes');
+const temperamentRoutes = require('./routes/temperamentRoutes');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-// GET all breeds
-app.get('/breeds', async (req, res) => {
-  try {
-    const [rows, fields] = await pool.query('SELECT * FROM breeds');
-    res.json(rows);
-  } catch (err) {
-    console.error('Error querying database:', err);
-    res.status(500).send('Error querying database');
-  }
-});
+// Routes for breeds
+app.use('/breeds', breedRoutes);
 
-// POST a new breed
-app.post('/breeds', async (req, res) => {
-  const { name, origin, temperament } = req.body;
-  try {
-    await pool.query('INSERT INTO breeds (name, origin, temperament) VALUES (?, ?, ?)', [name, origin, temperament]);
-    res.status(201).send('Breed added successfully');
-  } catch (err) {
-    console.error('Error inserting into database:', err);
-    res.status(500).send('Error inserting into database');
-  }
-});
+// Routes for temperaments
+app.use('/temperaments', temperamentRoutes);
 
-// PUT update a breed by ID
-app.put('/breeds/:id', async (req, res) => {
-  const { name, origin, temperament } = req.body;
-  const breedId = req.params.id;
-  try {
-    await pool.query('UPDATE breeds SET name = ?, origin = ?, temperament = ? WHERE id = ?', [name, origin, temperament, breedId]);
-    res.send('Breed updated successfully');
-  } catch (err) {
-    console.error('Error updating database:', err);
-    res.status(500).send('Error updating database');
-  }
-});
-
-// DELETE a breed by ID
-app.delete('/breeds/:id', async (req, res) => {
-  const breedId = req.params.id;
-  try {
-    await pool.query('DELETE FROM breeds WHERE id = ?', [breedId]);
-    res.send('Breed deleted successfully');
-  } catch (err) {
-    console.error('Error deleting from database:', err);
-    res.status(500).send('Error deleting from database');
-  }
+// Test route
+app.get('/', (req, res) => {
+  res.send('Connection successful!');
 });
 
 // Start the server
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
+
